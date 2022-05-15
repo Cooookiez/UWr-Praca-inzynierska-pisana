0f003e980fefed4c31a40ebbd9501e6a381d9d63
Krzysiek (Chris) <kukizk@gmail.com>
2022-05-15 17:52:12 +0200

    screenVisualization poprawiona wersja

--- code/screenVisualization.py
+++ code/screenVisualization.py
@@ -1,33 +1,43 @@
-# # # # # # # # # # # # #
-# TU SIE JESZCZE ZMIENI #
-# # # # # # # # # # # # #
-
 def screenVisualization():
-    global appLastMode
     global lastMods2Show
-    print(f"\t|{mods2ShowQueue}|")
+    global lGreeting
+    global lName
     if len(mods2ShowQueue) > 0:
         mods2ShowQueue.sort()
         # sprawdzenie wykorzystania limitu czasu
         if time.time() > mods2ShowQueue[0].time_end:
+            # pop
             mods2ShowQueue.pop(0)
+            lGreeting.pack_forget()
+            lName.pack_forget()
             screenVisualization()
-            pass
         else:
             activeMode2show = mods2ShowQueue[0]
             # zmien tylko gdy mod2show jest nowy
             if activeMode2show != lastMods2Show:
-                Tk.configure(background=appBackground[
-                activeMode2show.activeMode])
+                # kolor tła
+                Tk.configure(background=
+                    appBackground[activeMode2show.activeMode])
+                lGreeting.configure(background=
+                    appBackground[activeMode2show.activeMode])
+                lName.configure(background=
+                    appBackground[activeMode2show.activeMode])
+                
+                # zmien imie textu
+                lName.config(text=activeMode2show.name)
+                
+                # wyswietl text
+                lGreeting.pack()
+                lName.pack()
+                
                 Tk.update()
+                if activeMode2show.activeMode == Mods.UNKNOWN:
+                    say_hello("Unknown")
+                else:
+                    say_hello(activeMode2show.name)
                 lastMods2Show = activeMode2show
                 appLastMode = activeMode2show.activeMode;
-                pass
-            pass
-        pass
     else:
-        # zmien na idle (jesli juz nie jest)
-        if appLastMode != Mods.IDLE:
-            Tk.configure(background=appBackground[Mods.IDLE])
-            Tk.update()
-        appLastMode = Mods.IDLE;
+        # zmien na idle
+        Tk.configure(background=appBackground[Mods.IDLE])
+        Tk.update()
